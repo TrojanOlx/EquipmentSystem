f7259c3c0c88fa0a7ff3cb4299d53c34482b66ed
龙兴 欧 <815195981@qq.com>
2019-07-07 23:30:38 +0800

    整合代码

--- EquipmentSystem.Web/Views/Equipment/Index.js
+++ EquipmentSystem.Web/Views/Equipment/Index.js
@@ -26,3 +26,23 @@
             _$modal.find("input:not([type=hidden]):first").focus();
         });
 })();
+
+
+$("#file-1").fileinput({
+    uploadUrl: '/FileUpload/UploadImage', // you must set a valid URL here else you will get an error
+    allowedFileExtensions: ['jpg', 'png', 'gif'],
+    overwriteInitial: false,
+    maxFileSize: 1000,
+    maxFilesNum: 10,
+    //allowedFileTypes: ['image', 'video', 'flash'],
+    slugCallback: function (filename) {
+        return filename.replace('(', '_').replace(']', '_');
+    }
+});
+
+$("#file-1").on('fileuploaded', function (event, data, previewId, index) {
+    console.log(data);
+    if (typeof (data.response.id) != 'undefined') {
+        id_str = id_str + data.response.id + ',';
+    }
+});
